33ed01b53a65ae1c8efd30fe1c54a89c49a6ad89
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-03-02 16:55:22 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import * as Battery from 'expo-battery';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import ProgressCircle from 'react-native-progress-circle'
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { batteryLevel: null};
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    let batteryLevel = await this._get();
+    let b = await Battery.isLowPowerModeEnabledAsync();
+    console.log("3",b)
+    this.setState({ batteryLevel });
+    this._subscribe();
+  }
+  _bt = async () => {
+    let batteryLevel = await this._get();
+    this.setState({ batteryLevel });
+  }
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+  _get = async () => {
+    let a = await Battery.getBatteryLevelAsync();
+    return a;
+  }
+  
+
+  _subscribe = () => {
+    this._subscription = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+      this.setState({ batteryLevel });
+    });
+  };
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  render() {
+    let test, test2;
+    if (this.state.batteryLevel) {
+      let a  = Number((this.state.batteryLevel*100).toFixed(1));
+      test = <ProgressCircle
+            percent={Math.round(a)}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+            outerCircleStyle ={{width: 100}}
+        >
+            <Text style={styles.pin}>{a}</Text>
+        </ProgressCircle>
+        if (a > 0 && a <= 20) {
+            test2 = <Text style={styles.noteLow}>Pin yếu</Text>
+        } else if (a > 20 && a <= 80) {
+            test2 = <Text style={styles.noteMedium}>Pin đủ dùng</Text>
+        } else {
+            test2 = <Text style={styles.noteHigh}>Pin đầy</Text>
+        }
+    }
+    console.log("chin",this.state.batteryLevel);
+    return (
+      <View style={styles.container}>
+         {test}
+          {test2}
+         <TouchableOpacity
+          style={styles.button}
+          onPress={this._bt}
+        >
+          <Text> Nhấn </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pin: {
+    fontSize: 40
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 30,
+    margin: 50,
+    fontSize: 50
+  },
+  noteLow : {
+    fontSize: 50,
+    color:'#f58d42'
+  },
+  noteMedium : {
+    fontSize: 50,
+    color:'#5a8544'
+  },
+  noteHigh : {
+    fontSize: 50,
+    color:'#7295b8'
+  }
+ 
+});
